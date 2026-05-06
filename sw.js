d8699cc173e1ad5a598c345a93bb26597c9c2bfc
MrSteak Admin <admin@mrsteak.com>
2026-05-06 17:21:26 +0700

    Add PWA support (Manifest, Service Worker, Icons)

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'english-learning-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './css/styles.css',
+  './js/app.js',
+  './js/data.js',
+  './manifest.json',
+  './icons/icon.svg',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Prompt:wght@300;400;500;600;700&display=swap'
+];
+
+// Install a service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return response from cache if found, otherwise fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Update a service worker
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
